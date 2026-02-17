0fccd1b38418f762cb40c91c5ea124486b8847a1
bossanova1986 <sbosse@ovgu.de>
2026-02-17 09:34:26 +0100

    config module

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,9 +3,15 @@ import { AuthController } from './controller/auth.controller';
 import { LogoutGuard } from './services/logout.guard';
 import { SamlLoginGuard } from './services/saml-login.guard';
 import { SamlStrategy } from './strategies/saml.strategy';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+      envFilePath: [(process.env.NODE_ENV) ? `env.${process.env.NODE_ENV}` : 'env.template'],
+    })
+  ],
   controllers: [AuthController],
   providers: [LogoutGuard, SamlLoginGuard, SamlStrategy],
 })
